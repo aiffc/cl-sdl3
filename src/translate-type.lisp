1411d52304eb52a5821180e0c9e271c7a207c53e
Bibek Panthi <bpanthi977@gmail.com>
2025-02-08 03:11:21 -0600

    Fix infinite recursion in defcenum due to typo

--- src/translate-type.lisp
+++ src/translate-type.lisp
@@ -90,7 +90,7 @@
   (let ((name (first-or-identity name-and-options))
         (keywords (mapcar #'first-or-identity enum-list)))
     `(eval-when (:compile-toplevel :load-toplevel :execute)
-       (defcenum ,name-and-options
+       (cffi:defcenum ,name-and-options
          ,@enum-list)
        (deftype ,name ()
          `(member ,,@keywords)))))
